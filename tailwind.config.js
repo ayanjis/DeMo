339bb436efc8f5f095ddc157d72da86a6652ee05
ayanjis <ayancdas@gmail.com>
2024-07-25 00:40:53 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'Group_26': "url('/IMG/Group26.png')",
+        'Group_25': "url('/IMG/Group25.png')",
+        'Group_24': "url('/IMG/Group24.png')",
+        'Group_23': "url('/IMG/Group23.png')",
+
+        'Rectangle21': "url('/IMG/Rectangle21.png')",
+        'Rectangle221': "url('/IMG/Rectangle221.png')",
+        'Rectangle213': "url('/IMG/Rectangle213.png')",
+        'Rectangle212': "url('/IMG/Rectangle212.png')",
+
+        'Mask_group': "url('/IMG/Mask_group.png')",
+        'Mask_group2': "url('/IMG/Mask_group-Recovered2.png')",
+      }
+    },
+  },
+  plugins: [],
+}
+
